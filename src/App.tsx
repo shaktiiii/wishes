4f3f2ee07d9501294342991686f6de90dafb8617
shaktidubey <dubeyshakti0827@gmail.com>
2023-12-30 19:21:26 +0530

    web3 modal button and font added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
+import { useWeb3Modal } from "@web3modal/wagmi/react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
+  const { open } = useWeb3Modal();
   return (
     <>
       <div className="flex flex-col gap-5 h-screen w-screen justify-center items-center ">
-        <button className="border w-[10em] border-black outline-none px-4 py-2 rounded-md"> Good Morning </button>
-        <button className="border w-[10em] border-black outline-none px-4 py-2 rounded-md">Happy NewYear</button>
+        <button className="border w-[10em] border-black outline-none px-4 py-2 rounded-md ">
+          Good Morning
+        </button>
+
+        <button
+          onClick={() => {
+            open();
+          }}
+          className="border w-[10em] border-black outline-none px-4 py-2 rounded-md"
+        >
+          Happy NewYear
+        </button>
       </div>
     </>
   );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+
+import { WagmiConfig } from "wagmi";
+import { polygonMumbai } from "viem/chains";
+
+const projectId = "904623217cd89ca1a411940339ad0a1c";
+
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [polygonMumbai];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+createWeb3Modal({ wagmiConfig, projectId, chains });
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig config={wagmiConfig}>
+      <App />
+    </WagmiConfig>
+  </React.StrictMode>
+);
