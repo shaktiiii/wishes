792822850841f49f743c51268699e20248dd391a
shaktidubey <dubeyshakti0827@gmail.com>
2023-12-30 16:01:51 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col gap-5 h-screen w-screen justify-center items-center ">
+        <button className="border w-[10em] border-black outline-none px-4 py-2 rounded-md"> Good Morning </button>
+        <button className="border w-[10em] border-black outline-none px-4 py-2 rounded-md">Happy NewYear</button>
+      </div>
+    </>
+  );
+}
+
+export default App
